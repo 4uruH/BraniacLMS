b8a25a5b5e19a42742461cb859c23223b8f56bce
4uruH <fantom-gtn@list.ru>
2022-09-19 16:49:51 +0300

    Lesson 2 commit

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from .views import *
 
-urlpatterns = [
-    path('hello/', helloview)
-]
+urlpatterns = {
+    path('', MainPageView.as_view()),
+    path('login.html/', LoginPageView.as_view()),
+    path('doc_site.html/', DocSitePageView.as_view()),
+    path('contacts.html/', ContactsPageView.as_view()),
+    path('courses_list.html', CoursesPageView.as_view()),
+    path('news.html/', NewsPageView.as_view())
+}
