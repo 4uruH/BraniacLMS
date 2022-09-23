3c0fdf6f053efd6d271127227366036720c7b603
4uruH <fantom-gtn@list.ru>
2022-09-23 22:12:12 +0300

    semi commit

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from .views import *
 
 urlpatterns = [
-    path('', MainPageView.as_view(), name='index'),
+    path('', MainPageView.as_view(), name='main'),
     path('login/', LoginPageView.as_view(), name='login'),
     path('doc_site/', DocSitePageView.as_view(), name='doc_site'),
     path('contacts/', ContactsPageView.as_view(), name='contacts'),
