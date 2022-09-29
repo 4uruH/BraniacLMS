516a70d21e857ab3370bd9d0417ef2f00aa1987c
4uruH <97702288+4uruH@users.noreply.github.com>
2022-09-29 15:15:16 +0300

    DZ4 Done

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('doc_site/', DocSitePageView.as_view(), name='doc_site'),
     path('contacts/', ContactsPageView.as_view(), name='contacts'),
     path('courses_list/', CoursesPageView.as_view(), name='courses_list'),
-    path('news/', NewsPageView.as_view(), name='news')
+    path('news/', NewsPageView.as_view(), name='news'),
+    path("news/<int:pk>/", NewsPageDetailView.as_view(), name="news_detail"),
+    path("courses/<int:pk>/", CoursesDetailView.as_view(), name="courses_detail")
 ]
